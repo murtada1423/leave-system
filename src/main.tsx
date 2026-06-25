f48b9eb8585f95d29ad3cbe9495b406b0a87052a
murtada1423 <murtada1423@gmail.com>
2026-06-25 05:08:01 +0300

    1.6

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then((reg) => {
+      console.log('Service Worker Registered!', reg.scope)
+    }).catch((err) => {
+      console.log('Service Worker Failed!', err)
+    })
+  })
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
